e4596e582d2e55fbb74b5c26426f32901ec4e426
AdamYih <adamyih5403@gmail.com>
2018-04-26 12:45:03 +0800

    remote: Compressing objects: 100% (134/134), done. remote: Total 158 (delta 29), reused 147 (delta 18), pack-reused 0 Receiving objects: 100% (158/158), 13.96 MiB | 4.86 MiB/s, done. Resolving deltas: 100% (29/29), completed with 3 local objects. From https://github.com/mingsterism/ada    f3f5c8b..70deeb2  Hibiki     -> origin/Hibiki    899ab2e..9a2773d  Ming       -> origin/Ming

---
+++ adam/day3/evenodd.js
@@ -0,0 +1,15 @@
+
+
+function oddEven(someInput) {
+	if (someInput % 2 === 0)
+	{	return true;}
+	else {
+		return false;}
+
+function mainFunction (number) {if(oddEven(number) == "Even")
+{console.log("the number", number, "is Even")}
+else {
+  console.log("the number", number, "is odd")
+}
+}
+malFunction(20)
