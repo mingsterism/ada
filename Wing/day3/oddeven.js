b55711fc49be192691b8bf61fec254e707bec53e
Ming <ming.k@hotmail.com>
2018-04-26 12:41:25 +0800

    Improved oddeven function yaaa

--- Wing/day3/oddeven.js
+++ Wing/day3/oddeven.js
@@ -1,7 +1,21 @@
-let number = 6
+function isEven (number) {
+    if (number % 2 == 0) {
+        return true
+    }  else {
+        return false
+    }
+}
 
-if (number % 2 === 0){
-console.log('Even');}
-else {
-console.log ("Odd")
-};
+function mainFunction (number) {
+    if (isEven(number)) {
+        console.log("The number", number, "is Even")
+        console.log("blablabal")
+        console.log("blablabal")
+        console.log("blablabal")
+        console.log("blablabal")
+    } else {
+        console.log("The nuber", number, "is Odd")
+    }
+}
+
+mainFunction(20)
